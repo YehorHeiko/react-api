7c223074d17cc727fd2cf57414e8c109465f9d56
Yehor Heiko <yehorheiko.developer@gmail.com>
2024-10-27 00:52:11 +0300

    Make some propertice

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,9 +1,20 @@
+import axios from "axios";
+import React, { useState } from "react";
+
+
+
+const url = "https://content.guardianapis.com/search?page=2&q=debate&api-key=test";
+
 function App() {
-  return ( 
+  const [res, serRes] = useState(1);
+  
+
+  
+  return (
     <div>
-      <h1>hello world</h1>
+      <h1>{res}</h1>
     </div>
-   );
+  );
 }
 
 export default App;
