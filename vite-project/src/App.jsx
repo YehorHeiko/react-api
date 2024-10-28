6d97a19d38236f16cfdb955f29698d24626f6f04
Yehor Heiko <yehorheiko.developer@gmail.com>
2024-10-28 14:51:24 +0300

    Traning

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,20 +1,31 @@
-import axios from "axios";
-import React, { useState } from "react";
-
-
-
-const url = "https://content.guardianapis.com/search?page=2&q=debate&api-key=test";
+import React, { useState, useEffect } from "react";
+import axios from "axios"; // Подключаем Axios
 
 function App() {
-  const [res, serRes] = useState(1);
-  
+  const [statiy, setStatiy] = useState([]);
+  const [newStatiy, setNewStatiy] = useState({ title: "", body: "" });
 
-  
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        setStatiy(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  });
+
   return (
     <div>
-      <h1>{res}</h1>
+      <h1>
+        {statiy.map((e) => (
+          <h1 key={e.id}>{e.id}</h1>
+          <div>{e.value}</div>
+          <div>{e.target}</div>
+        ))}
+      </h1>
     </div>
   );
 }
-
 export default App;
